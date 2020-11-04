4299ac2753e9b3ef06afb865ecea7b5e74fe9f17
Mykooker <51872611+Mykooker@users.noreply.github.com>
2020-11-04 17:24:24 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+# 这是开源版本
+
+# 易支付API地址, 末尾需要包含/
+API = 'https://home.bakbak.cn/'
+# 商户ID
+ID = 12123
+# 商户密钥
+KEY = 'DZNEk2dQzKD10NCX0RwR'
+
+# 支付成功跳转地址
+JUMP_URL = "https://kangle.bakbak.cn/paysuccess.html"
+# 支付超时时间(秒)
+PAY_TIMEOUT = 300
+# BOT API
+TOKEN = '1469787690:AAEFt6qadwITeW6sWBl6Eq8Y-XHUYXGm884'
+# ADMIN ID
+ADMIN_ID = [755860119]
+
+# 管理员命令
+ADMIN_COMMAND_START = 'k'
+ADMIN_COMMAND_QUIT = 't'
